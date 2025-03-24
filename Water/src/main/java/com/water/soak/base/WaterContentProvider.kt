aff7efadd4fb6003f8779afbea9df4c56a0ad307
huangtianyun <352496193@qq.com>
2025-03-24 14:27:30 +0800

    hide provider

--- Water/src/main/java/com/water/soak/base/WaterContentProvider.kt
+++ Water/src/main/java/com/water/soak/base/WaterContentProvider.kt
@@ -10,41 +10,41 @@ import com.water.soak.SoakImpl
  * Date：2024/8/12
  * Describe:
  */
-class WaterContentProvider : ContentProvider() {
-    override fun onCreate(): Boolean {
-        return true
-    }
-
-    override fun query(
-        uri: Uri,
-        projection: Array<out String>?,
-        selection: String?,
-        selectionArgs: Array<out String>?,
-        sortOrder: String?
-    ): Cursor? {
-        context?.let {
-            return SoakImpl(
-                it, "shortcutSupport", "photoSupport", context?.packageName ?: "", uri
-            ).getMyCursor()
-        }
-        return null
-    }
-
-    override fun getType(uri: Uri): String? {
-        return null
-    }
-
-    override fun insert(uri: Uri, values: ContentValues?): Uri? {
-        return null
-    }
-
-    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
-        return 0
-    }
-
-    override fun update(
-        uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?
-    ): Int {
-        return 0
-    }
-}
+//class WaterContentProvider : ContentProvider() {
+//    override fun onCreate(): Boolean {
+//        return true
+//    }
+//
+//    override fun query(
+//        uri: Uri,
+//        projection: Array<out String>?,
+//        selection: String?,
+//        selectionArgs: Array<out String>?,
+//        sortOrder: String?
+//    ): Cursor? {
+//        context?.let {
+//            return SoakImpl(
+//                it, "shortcutSupport", "photoSupport", context?.packageName ?: "", uri
+//            ).getMyCursor()
+//        }
+//        return null
+//    }
+//
+//    override fun getType(uri: Uri): String? {
+//        return null
+//    }
+//
+//    override fun insert(uri: Uri, values: ContentValues?): Uri? {
+//        return null
+//    }
+//
+//    override fun delete(uri: Uri, selection: String?, selectionArgs: Array<out String>?): Int {
+//        return 0
+//    }
+//
+//    override fun update(
+//        uri: Uri, values: ContentValues?, selection: String?, selectionArgs: Array<out String>?
+//    ): Int {
+//        return 0
+//    }
+//}
